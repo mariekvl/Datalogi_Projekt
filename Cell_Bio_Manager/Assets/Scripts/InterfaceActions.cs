a4f347ac221be2c7f86f3eb37265cc175e6a1084
MSI\marie <mariekvl@hotmail.com>
2025-11-17 12:40:42 +0100

    UI overlay started

---
+++ Cell_Bio_Manager/Assets/Scripts/InterfaceActions.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class InterfaceActions : MonoBehaviour
+{
+
+    [SerializeField]
+    private VisualTreeAsset _treeAsset = default;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void openShop()
+    {
+        
+    }
+}
